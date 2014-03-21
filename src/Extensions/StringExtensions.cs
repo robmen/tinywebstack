a73e8730779bcc92543c8c9eb32d4aa661d1385c
Rob Mensching <rob@robmensching.com>
2014-03-21 15:20:06 -0700

    Add support decoding HTML and URLs for strings.

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -1,9 +1,30 @@
 ﻿using System;
+using System.Web;
 
 namespace TinyWebStack.Extensions
 {
     internal static class StringExtensions
     {
+        public static string HtmlDecode(this string s)
+        {
+            return HttpUtility.HtmlDecode(s);
+        }
+
+        public static string HtmlEncode(this string s)
+        {
+            return HttpUtility.HtmlEncode(s);
+        }
+
+        public static string UrlDecode(this string s)
+        {
+            return HttpUtility.UrlDecode(s);
+        }
+
+        public static string UrlEncode(this string s)
+        {
+            return HttpUtility.UrlEncode(s);
+        }
+
         public static string WithTrailingSlash(this string s)
         {
             return s.EndsWith("/", StringComparison.Ordinal) ? s : s + "/";
